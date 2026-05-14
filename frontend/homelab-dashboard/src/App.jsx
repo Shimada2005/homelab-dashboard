8531cd7975fdbad9dd1ae9f8ad741ab0b796ee39
Shimada2005 <kazuto2005@icloud.com>
2026-05-14 13:27:48 +0900

    フロント更新　UIの最適化

--- frontend/homelab-dashboard/src/App.jsx
+++ frontend/homelab-dashboard/src/App.jsx
@@ -5,11 +5,31 @@ function App() {
   const [status, setStatus] = useState(null);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/status")
-      .then((res) => res.json())
-      .then((data) => setStatus(data))
-      .catch((error) => console.error(error));
+    const fetchStatus = () => {
+      fetch("http://127.0.0.1:8000/status")
+        .then((res) => res.json())
+        .then((data) => setStatus(data))
+        .catch((err) => console.error(err));
+    };
+
+    fetchStatus();
+    const interval = setInterval(fetchStatus, 1000);
+
+    return () => clearInterval(interval);
   }, []);
+
+  const renderBar = (value) => {
+    const totalBars = 10;
+    const filledBars = Math.round((value / 100) * totalBars);
+
+    return "█".repeat(filledBars) + "░".repeat(totalBars - filledBars);
+  };
+
+  const getColor = (value) => {
+  if (value < 50) return "#4ade80"; // green
+  if (value < 80) return "#facc15"; // yellow
+  return "#ef4444"; // red
+};
 
   return (
     <div className="container">
@@ -19,11 +39,30 @@ function App() {
         <div className="card-container">
           <div className="card">
             <h2>CPU Usage</h2>
-            <p>{status.cpu}%</p>
+
+            <p className="percent">{status.cpu}%</p>
+
+            <div className="bar-bg">
+              <div className="bar-fill" 
+              style={{ 
+                width: `${status.cpu}%` ,
+                backgroundColor: getColor(status.cpu)
+                }} />
+            </div>
           </div>
+
           <div className="card">
             <h2>Memory Usage</h2>
-            <p>{status.memory}%</p>
+
+            <p className="percent">{status.memory}%</p>
+
+            <div className="bar-bg">
+              <div className="bar-fill" 
+              style={{ 
+                width: `${status.memory}%` ,
+                backgroundColor: getColor(status.memory)
+                }} />
+            </div>
           </div>
         </div>
       ) : (
